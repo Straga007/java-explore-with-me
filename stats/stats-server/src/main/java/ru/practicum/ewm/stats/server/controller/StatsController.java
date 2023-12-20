98ab2f94abf202ec177a95a191fca90e297d28b1
Straga07 <Straga07@yandex.ru>
2023-12-21 01:55:04 +0300

    test

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/controller/StatsController.java
@@ -36,7 +36,7 @@ public class StatsController {
     }
 
     @PostMapping("/hit")
-    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseStatus(value = HttpStatus.CREATED)
     public HitDto addHit(@RequestBody @Valid HitDto hitDto) {
         log.info("Получаем запрос hitDto={}", hitDto);
         HitDto newHitDto = statsService.addHit(hitDto);
