db2b447ff38f72a7160fd4935c733b6b22a8f0bf
Straga07 <Straga07@yandex.ru>
2023-12-22 02:48:30 +0300

    after revue v1 test

--- main/src/main/java/ru/practicum/ewm/categories/service/CategoryService.java
+++ main/src/main/java/ru/practicum/ewm/categories/service/CategoryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CategoryService {
     Category getCategoryByIdNotDto(Long categoryId);
+
     List<CategoryDto> getCategories(Integer from, Integer size);
 
     CategoryDto getCategoryById(Long categoryId);
